f940528c11ce545783423ad998ffa2bf1fc70c87
LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>
2022-11-05 15:32:05 -0300

    Feat get values task of form
    Signed-off-by: LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>

--- src/routes/Home.svelte
+++ src/routes/Home.svelte
@@ -1,3 +1,32 @@
+<script>
+  let getValuesTaskForm = {
+    title: '',
+    description: '',
+  };
+
+  function handleSubmitForm() {
+    console.log(getValuesTaskForm);
+  }
+</script>
+
 <section>
-  <h1 class="text-3xl font-light underline">Hello world!</h1>
+  <form on:submit|preventDefault={handleSubmitForm}>
+    <label for="title">Title</label>
+    <input
+      type="text"
+      placeholder="Write a title"
+      bind:value={getValuesTaskForm.title}
+    />
+
+    <label for="description">Description</label>
+    <textarea
+      name="description"
+      id="description"
+      rows="3"
+      placeholder="Write a description"
+      bind:value={getValuesTaskForm.description}
+    />
+
+    <button> save </button>
+  </form>
 </section>
