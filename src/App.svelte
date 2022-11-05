3a152fb61ab5e3298f31625beedea529750d8268
LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>
2022-11-05 14:52:59 -0300

    Feat base of project
    Signed-off-by: LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>

---
+++ src/App.svelte
@@ -0,0 +1,12 @@
+<script>
+  import { Router, Route } from 'svelte-navigator';
+  import Home from './routes/Home.svelte';
+</script>
+
+<Router>
+  <main>
+    <Route path="/">
+      <Home />
+    </Route>
+  </main>
+</Router>
---
+++ src/routes/Home.svelte
@@ -0,0 +1,3 @@
+<section>
+  <h1 class="text-3xl font-light underline">Hello world!</h1>
+</section>
